0114e67830815ecae4e8a6815eb83c5ab2875574
Akshay Sharma <akshaya.sharma770@gmail.com>
2025-10-16 21:20:44 +0530

    ux design

--- linked-list/linked-list-visualizer/src/App.tsx
+++ linked-list/linked-list-visualizer/src/App.tsx
@@ -5,7 +5,7 @@ import ControlsPanel from './components/ControlsPanel'
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-8 pt-6 pb-10">
+    <div className="max-w-7xl mx-auto p-4">
       <Header />
       <VisualizationCanvasCard />
       <ControlsPanel />
